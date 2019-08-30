887f9e81a8cd4abd5eff2638149f165eb498691f
satonin18 <satonin18@mail.ru>
2019-08-30 13:02:49 +0500

    v1-передача JSON
    ver1 (Angular and Java)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+// import { HttpService} from './http.service';
+import {HttpClient} from '@angular/common/http';
+import {User} from './user';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // providers: [HttpService]
 })
-export class AppComponent {
-  title = 'ngWeb';
+export class AppComponent implements OnInit {
+
+  user: User;
+  // users: User[] = [];
+  response: any;
+
+  constructor(private http: HttpClient) { }
+  // constructor(private httpService: HttpService) {}
+
+  ngOnInit() {
+    // this.http.get('user.json') //Observable<any>
+    // this.httpService.getUsers()
+    // .subscribe((data:User) => this.user=data);
+    // .subscribe(data => this.users=data["userList"]);
+    //   .subscribe(data => this.users = data);
+
+    const observable: Observable<User> = this.http.get<User>('http://localhost:8081/lkz_project_war_exploded/angular/user');
+    observable.subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+/*
+declarations: классы представлений (view classes), которые принадлежат модулю.
+    Angular имеет три типа классов представлений: компоненты (components), директивы (directives), каналы (pipes)
+exports: набор классов представлений, которые должны использоваться в шаблонах компонентов из других модулей
+imports: другие модули, классы которых необходимы для шаблонов компонентов из текущего модуля
+providers: классы, создающие сервисы, используемые модулем
+bootstrap: корневой компонент, который вызывается по умолчанию при загрузке приложения
+*/
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule], // HttpModule = depricated
+  declarations: [ AppComponent], // all in here
+  bootstrap:    [ AppComponent ],
   providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
